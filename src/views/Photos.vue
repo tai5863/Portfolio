bab5d62332b0aff6be8182ba384ebaf9a3ab9266
tai5863 <taisws5863@gmail.com>
2020-04-22 01:03:32 +0900

    [FIX] recommit

--- src/views/Photos.vue
+++ src/views/Photos.vue
@@ -2,7 +2,7 @@
   <div class="photos">
     <h2 class="photos_title">Photos</h2>
     <div class="message_container begin">
-      <p class="message begin">これは、僕が好きな景色たちです。</p>
+      <p class="message begin">これは 、僕が好きな景色たちです。</p>
     </div>
     <div id="photos_container">
       <ul>
